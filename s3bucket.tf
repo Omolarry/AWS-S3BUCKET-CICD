3327b58b1d2df33f4a550f5813000afff1f046ef
Omolarry <tomikefamoroti@gmail.com>
2023-03-30 12:38:08 +0100

    initial commit

---
+++ s3bucket.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "mywebsitesource-test-bucket"
+  
+  tags = {
+    Name = "Source bucket"
+    Environment = "Test"
+  }
+   versioning {
+    enabled = false
+  }
+}
+
+resource "aws_s3_bucket" "bucket2" {
+  bucket = "mywebsiteprod"
+  
+  tags = {
+    Name = "Prod bucket "
+    Environment = "Production"
+  }
+
+   versioning {
+    enabled = false
+  
+  }
+}
